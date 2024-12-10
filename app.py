11521115ccd8c7c241afe7bc53c8424a7a3e6e93
suriyag2002 <suriya.gunasekaran2002@example.com>
2024-12-11 05:21:42 +0530

    first commit

--- app.py
+++ app.py
@@ -30,6 +30,5 @@ def get_jobs():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    # Bind to the port specified by Render
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
